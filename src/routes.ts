2bee7ce3b8e499e1a87ece5857520a3bd5854c77
Murilio <muriliosilva201@gmail.com>
2021-03-08 22:15:55 -0300

    adding commitlint

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import UsersController from '@controllers/UsersController'
 
 const routes = Router()
 
+routes.get('/', (req, res) => { return res.json({ message: 'hello my setup node' }) })
+
 routes.get('/users', UsersController.index)
 
 routes.post('/users', UsersController.store)
